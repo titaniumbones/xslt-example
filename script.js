58df2170358eed86e6f5b299b0691b6044317919
Matt Price <matt.price@utoronto.ca>
2019-09-26 12:41:08 -0400

    changes to ruin.xml & script.js from May 2019
    - improved encoding of poem
    - let -> const in script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let syns = document.querySelectorAll("span.synonym");
 // you could instead just do a reagular for---of loop
 syns.forEach(thisSpan => {
   // wouldn eed to replace with a real synonym maker
-  let ran = Math.floor(Math.random() * 10 );
-  let apiCall = jQuery.get('https://jsonplaceholder.typicode.com/todos/' + ran,  // don't always return the exact same phrase
+  const ran = Math.floor(Math.random() * 10 );
+  const apiCall = jQuery.get('https://jsonplaceholder.typicode.com/todos/' + ran,  // don't always return the exact same phrase
                            null, // no error handling
 
                            // callback function in which we set the content of the span to a part of the api response
@@ -19,5 +19,5 @@ syns.forEach(thisSpan => {
                              thisSpan.innerHTML = response.title; // what you put on the righthand side of the equals sign will depend on your API
                            },
                            'json');
- 
+  
 });
